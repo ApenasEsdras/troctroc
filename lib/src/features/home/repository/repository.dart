2feec72b06e0645bdf91ff7095408cae92c0b251
esdras.de.oliveira <apenasesdras@gmail.com>
2024-06-23 21:32:22 -0300

    ontendo o nome do proejto logado

--- lib/src/features/home/repository/repository.dart
+++ lib/src/features/home/repository/repository.dart
@@ -1,16 +1,22 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<String> getProjectName() async {
-  // Supondo que o nome do projeto está armazenado em um documento no Firestore
-  // com o caminho 'projects/currentProject'
-  final DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
-      .collection('projects')
-      .doc('currentProject')
-      .get();
+  try {
+    final DocumentSnapshot documentSnapshot = await FirebaseFirestore.instance
+        .collection('projects')
+        .doc('currentProject')
+        .get();
 
-  if (documentSnapshot.exists) {
-    return documentSnapshot['projectName']; // substitua 'projectName' pelo campo correto
-  } else {
-    throw Exception('Projeto não encontrado');
+    if (documentSnapshot.exists) {
+      final projectName = documentSnapshot.get('projectName'); // substitua 'projectName' pelo campo correto
+      print('Project name retrieved: $projectName');
+      return projectName;
+    } else {
+      throw Exception('Projeto não encontrado');
+    }
+  } catch (e) {
+    print('Erro ao obter o nome do projeto: $e');
+    rethrow;
   }
 }
--- lib/src/features/home/screen/home_screen.dart
+++ lib/src/features/home/screen/home_screen.dart
@@ -1,8 +1,10 @@
+
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 
 import '../repository/repository.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
